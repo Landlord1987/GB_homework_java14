0a61cd0407c52623a484ed6aa1cef1b26338426b
DenisKrinitsyn <skylinekrin@gmail.com>
2022-12-04 21:38:23 +0500

    Homework14 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,66 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] m1 = {0, 1, 2, 4, 0, 5, 6};
+        int[] m2 = {0, 1, 2, 4, 0, 5, 6, 5, 22, 12};
+        int[] m3 = {0, 1, 2, 5, 0, 5, 6};
+        try {
+            System.out.println(Arrays.toString(getMas(m1)));
+            System.out.println(Arrays.toString(getMas(m2)));
+            System.out.println(Arrays.toString(getMas(m3)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int[] m11 = {1, 1, 1, 1, 1, 1, 1};
+        int[] m22 = {4, 4, 4, 4, 4, 4, 4};
+        int[] m33 = {1, 4, 1, 4, 4, 1, 4};
+        int[] m44 = {1, 1, 4, 4, 3, 4, 4};
+        System.out.println(getMas2(m11));
+        System.out.println(getMas2(m22));
+        System.out.println(getMas2(m33));
+        System.out.println(getMas2(m44));
+
+
+    }
+
+    public static int[] getMas(int[] arr) {
+        int four = -1;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 4) {
+                four = i;
+                break;
+            }
+        }
+        if (four == -1) {
+            throw new RuntimeException("В массиве нет ни одной 4: " + Arrays.toString(arr));
+        }
+
+        int[] arr1 = new int[arr.length - four - 1];
+
+        for (int i = four + 1, w = 0; i < arr.length; i++, w++) {
+            arr1[w] = arr[i];
+        }
+
+        return arr1;
+    }
+
+    public static boolean getMas2(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 1) {
+                if (arr[i] != 4) return false;
+            }
+            sum += arr[i];
+        }
+        if (sum == arr.length || sum == (4 * arr.length)) return false;
+
+        return true;
     }
 }
+
---
+++ src/test/java/GB_homework_java14/MainTest.java
@@ -0,0 +1,94 @@
+package GB_homework_java14;
+
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void getMasTest1(){
+        int[] m1 = {0, 1, 2, 4, 0, 5, 6};
+
+        int[] actual1 = Main.getMas(m1);
+
+        int[] expected1 = {0, 5, 6};
+
+        Assertions.assertArrayEquals(expected1, actual1);
+
+    }
+
+    @Test
+    public void getMasTest2(){
+
+        int[] m2 = {0, 1, 2, 4, 0, 5, 6, 5, 22, 12};
+
+        int[] actual2 = Main.getMas(m2);
+
+        int[] expected2 = {0, 5, 6, 5, 22, 12};
+
+        Assertions.assertArrayEquals(expected2, actual2);
+    }
+
+    @Test
+    public void getMasTest3(){
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            int[] m3 = {0, 1, 2, 5, 0, 5, 6};
+
+            int[] actual3 = Main.getMas(m3);
+
+            int[] expected3 = {0, 5, 6};
+
+            Assertions.assertArrayEquals(expected3, actual3);
+        });
+    }
+
+    @Test
+    public void getMasTest4NotNull(){
+
+        int[] m2 = {0, 1, 2, 4, 0, 5, 6, 5, 22, 12};
+
+        Assertions.assertNotNull(Main.getMas(m2));
+    }
+
+    @Test
+    public void getMas2Test1() {
+        int[] m11 = {1, 1, 1, 1, 1, 1, 1};
+
+        boolean actual = Main.getMas2(m11);
+        Assertions.assertEquals(false, actual);
+    }
+
+    @Test
+    public void getMas2Test2() {
+        int[] m22 = {4, 4, 4, 4, 4, 4, 4};
+
+        boolean actual = Main.getMas2(m22);
+        Assertions.assertEquals(false, actual);
+    }
+
+    @Test
+    public void getMas2Test3() {
+        int[] m33 = {1, 4, 1, 4, 4, 1, 4};
+
+        boolean actual = Main.getMas2(m33);
+        Assertions.assertEquals(true, actual);
+    }
+
+    @Test
+    public void getMas2Test4() {
+        int[] m44 = {1, 1, 4, 4, 3, 4, 4};
+
+        boolean actual = Main.getMas2(m44);
+        Assertions.assertEquals(false, actual);
+    }
+
+    @Test
+    public void getMas2Test5NotNull(){
+
+        int[] m2 = {0, 1, 2, 4, 0, 5, 6, 5, 22, 12};
+
+        Assertions.assertNotNull(Main.getMas(m2));
+    }
+}
